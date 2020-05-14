9e945faebfa8d1ea54d96f41487f265016ee3bf2
sumichaaan <yoshifumi.sumida@okinawaopenlabs.org>
2020-05-15 00:15:57 +0900

    Added test script to run nrf

---
+++ images/f5gc-nrf/tests/run.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run --rm \
+	-it \
+	--net=host \
+        -v $(pwd)/configs:/opt/f5gc/etc \
+        -v $(pwd)/configs:/opt/config \
+        -v $(pwd)/certs:/opt/support/TLS \
+        local/f5gc-nrf:latest
