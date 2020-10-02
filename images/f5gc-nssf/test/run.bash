1fc81e00ddfe814c8ec88efdb56241b22cffc8ff
sumichaaan <yoshifumi.sumida@okinawaopenlabs.org>
2020-10-02 20:54:27 +0000

    Update image version to v3.0.4

--- images/f5gc-nssf/test/run.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-	-it \
-	--net=host \
-        -v $(pwd)/../configs:/opt/f5gc/etc \
-        -v $(pwd)/../configs:/opt/config \
-        -v $(pwd)/../certs:/opt/support/TLS \
-        local/f5gc-nssf:latest
