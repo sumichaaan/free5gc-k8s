8c0b6ec32d2d50aca6861e8b9410b921ff245ab3
sumichaaan <yoshifumi.sumida@okinawaopenlabs.org>
2021-01-13 18:41:41 +0900

    Clean cni directories for each case

--- clusters/cni/case2/tungstenfabric/set_labels.sh
+++ clusters/cni/case2/tungstenfabric/set_labels.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# For All-In-One environment
 masternodes=$(kubectl get node | grep -w master | awk '{print $1}')
 agentnodes=$(kubectl get node | grep -v -w -e master -e NAME | awk '{print $1}')
-for i in config configdb analytics webui control agent analyticsdb 
+for i in config configdb analytics webui control
 do
  for masternode in ${masternodes}
  do
